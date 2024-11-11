f089d8c2a34ae0a71594dbedf7b6f6792475b6e3
API65I <amolnart@gmail.com>
2024-11-11 18:41:27 +0100

    visu

--- visualization/plot.py
+++ visualization/plot.py
@@ -1,13 +1,15 @@
 # visualization/plot.py
+
 # Szükséges könyvtárak importálása
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Függvény a logaritmikus adatokat megjelenítő grafikon létrehozására
+# Függvény a logaritmikus adatokat megjelenítő grafikon létrehozására, reziduálisokkal
 def create_plot(x_log, y_log, y_pred_log, mse, r2):
     # Biztosítjuk, hogy x_log és y_log NumPy tömbökké legyenek konvertálva
     x_log = np.array(x_log)
     y_log = np.array(y_log)
+    y_pred_log = np.array(y_pred_log)
     
     # Létrehozunk egy új ábrát és tengelyeket, 9x9-es méretben
     fig, ax = plt.subplots(figsize=(9, 9))
@@ -20,6 +22,10 @@ def create_plot(x_log, y_log, y_pred_log, mse, r2):
     
     # Regressziós egyenes megjelenítése
     ax.plot(x_log, y_pred_log, color="red", lw=2.5, label="Regressziós egyenes")
+    
+    # Reziduálisok megjelenítése: fekete színű, szaggatott vonalak az adatpontok és a regressziós egyenes között
+    for i in range(len(x_log)):
+        ax.plot([x_log[i], x_log[i]], [y_log[i], y_pred_log[i]], color='black', linestyle='--', lw=1, label="Reziduálisok" if i == 0 else "")
     
     # MSE és R² értékek kiírása a grafikonon
     ax.text(0.05, 0.95, f'MSE: {mse:.4f}\nR²: {r2:.4f}', transform=ax.transAxes, 
