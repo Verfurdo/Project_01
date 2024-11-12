2d34eb74bf66474ec6e03933c79b08326fd84f04
API65I <amolnart@gmail.com>
2024-11-12 08:39:10 +0100

    mod

--- project/project.py
+++ project/project.py
@@ -3,10 +3,10 @@ import streamlit_module
 
 # Fő függvény definiálása
 def main():
-    # Meghívja a run_streamlit_app() függvényt a streamlit_module fájlból
-    streamlit_module.run_streamlit_app() 
+    # Meghívja a streamlit_futtatasa függvényt a streamlit_module fájlból
+    streamlit_module.streamlit_futtatasa() 
 
 # Ellenőrizzük, hogy a fájlt közvetlenül futtatjuk-e
 if __name__ == "__main__":  # Ha a fájlt közvetlenül futtatjuk, akkor a __name__ értéke "__main__"
     main()  # Ebben az esetben meghívjuk a main() függvényt
-
+    
